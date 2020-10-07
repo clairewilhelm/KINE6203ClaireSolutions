43716f4b1f8e8c8b957b2a37d45681e5b99e4302
clairewilhelm <clairewilhelm98@gmail.com>
2020-10-07 14:35:08 -0400

    Update assignment1.m

--- Assignment 1/assignment1.m
+++ Assignment 1/assignment1.m
@@ -22,11 +22,11 @@
 
 % 1. Set the url of the matlab documentation to a variable called helpURL
 
-
+helpURL = "https://www.mathworks.com/help/matlab/"
 
 % 2. What does the command 'clear all' do?
 %
-% Answer: 
+% Answer: clears the workspace
 %
 
 
@@ -34,16 +34,18 @@
 % numbers [1 7 21 32 67 32453]? Store your answer in a variable called
 % averageAnswer.
 
+X = [1 7 21 32 67 32453] 
+averageAnswer=mean(X)
 
 
 % 4. What command would you type to see a list of all installed toolboxes?
 
-
+ver
 
 % 5. What command would return a list of all of the available functions in 
 % the image processing toolbox? 
 
-
+availableFunctions="https://www.mathworks.com/help/images/referencelist.html?type=function&s_tid=CRUX_topnav"
 
 % 6. On the line below figure, write a command to draw a graph of 
 % x and y. 
@@ -52,7 +54,7 @@ x = linspace(-2*pi,2*pi);
 y = sin(x);
 
 figure
-        %<- command goes here
+line(x,y)        %<- command goes here
 
         
 
@@ -60,14 +62,16 @@ figure
 % in a journal article (there are many possible answers to this). 
 
 %
-% Answer: 
+% Answer: ttest(x)
 %
 
 % 8. What are the 2 major types of loops that MATLAB supports? What is the
 % difference between them?
 
 %
-% Answer: 
+% Answer: The major types of loops is 'for' and 'while'. The difference is
+% 'for' tells matlab to repeat a certain amount of times, 'while' tells
+% matlab to repeat only when true. 
 % 
 
 
@@ -75,7 +79,8 @@ figure
 % an answer. 
 
 %
-% Answer: 
+% Answer: NaN stands for Not-a-Number. Would get this as an answer when an
+% operation has a result such as 0/0 or something equivalent to infinity. 
 %
 
 
@@ -85,69 +90,85 @@ figure
 % any of the values in A.
 
 A = [1 2 3 2 1 NaN 1 2 3 NaN];
-S = sum(A)
+S = sum(A,'omitnan')
 
 
 % 11. I used to love playing the game minesweeper. Is there a way that I
 % could play a similar game on Matlab? Write the command that would launch
 % the game if so. 
 
-
+xpbombs
 
 
 % 12. Create a variable myage and store your age on the day the hurricane is 
 % supposed to impact our area in it. Subtract two from the value of the
 % variable. Add one to the value of the variable. 
+myage=22 
+myage=(myage-2)+1
 
 
 % 13. Use the built-in function namelengthmax to find out the maximum number of
 % characters that you can have in an identifier name under your version of
 % MATLAB. Store the value in a variable called myMaxNameLength
-
+myMaxNameLength=namelengthmax
 
 % 14. Create two variables to store a weight in pounds and ounces. Use who and whos
 % to see the variables. Clear one of them and then use who and whos again.
-
-
+P=10
+Z=160
+who
+whos
+clear P
+who
+whos
 
 % 15. Store a number with a decimal place in a double variable (the default). Convert the
 % variable to the type int32 and store the result in a new variable (hint: this is known
 % as casting in computer programming).
-
+var=2.34
+val=int32(var)
 
 
 % 16. Create a variable called pounds to store a weight in pounds. Convert this to 
 % kilograms and assign the result to a variable kilos.
 
+pounds=10
+kilos= pounds/2.2
 
 
 % 18. Create a variable fTemp to store a temperature in degrees Fahrenheit (F). 
 % Convert this to degrees Celsius (C) and store the result in a variable cTemp.
 
-
+fTemp=45
+cTemp=((fTemp-32)*5)/9
 
 % 19. Using the colon operator, create the following row vectors:
 % 2 3 4 5 6 7 
 % 1.1000 1.3000 1.5000 1.7000 
 % 8 6 4 2
 % Store your results with variable names vecA, vecB and vecC respectively.
-
+vecA=[2:7]
+vecB=[1.1:0.2:1.7]
+vecC=[8:-2:2]
 
 % 20. Give the MATLAB expression that would create a vector 
 % (in a variable called vec) of 50 elements that range, equally spaced, 
 % from 0 to 2pi:
-
+vec=linspace(0,2*pi,50)
 
 
 % 21. Using the colon operator and the transpose operator, 
 % create a column vector that has the values -1 to 1 in steps of 0.5.
-
+cvec=[-1:0.5:1]
+cvec=cvec'
 
 % 22. Create a variable called rows that is a random integer in the inclusive range 
 % from 1 to 5. Create a variable called cols that is a random integer in the 
 % inclusive range from 1 to 5. Create a matrix called result of all zeros with the 
 % dimensions given by the values of rows and cols. 
-
+rows=randi([1 5],1)
+cols=randi([1 5],1)
+result=zeros(rows,cols)
 
 
 % 23. Create a vector of five random integers, each in the inclusive range 
@@ -157,6 +178,11 @@ S = sum(A)
 % - count how many are positive
 % - get the absolute value of each element
 % - find the maximum.
+vecf=randi([-10 10],1,5)
+vecfsubtract=vecf-3
+vecfpositive=sum(vecf>0)
+vecfabsolutevalue=abs(vecf)
+vecfmax=max(vecf)
 
 
 
@@ -165,6 +191,12 @@ S = sum(A)
 % result of your calculation in a variable called trapArea. Comment your
 % code so that another user can understand what your code does. 
 
+base1=10
+base2=7
+height=5
+trapArea=((base1+base2)/2)*height
+% trapArea finds the area of a trapezoid with a base of 10 and 7 and a
+% height of 5
 
 
 % 25. In biomechanics, a vector is a set of numbers that indicate 
@@ -182,6 +214,10 @@ S = sum(A)
 % calculates the unit vector and stores the value in a variable called
 % unitVector. 
 
+x=input('Enter x value here: ')
+y=input('Enter y value here: ')
+z=input('Enter z value here: ')
+unitVector= [x,y,z]/sqrt(x^2 + y^2 + z^2)
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%  Part 2: Hello World %%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -195,7 +231,9 @@ S = sum(A)
 
 %%% Enter the code for your program below this line
 
+disp('Hello World')
 
+A=input('insert phrase here: ')
 
 
 
